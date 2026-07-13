b23be287c6e81ba95bb2c66e53534e1ac4438806
MaxJack820 <365453574@qq.com>
2026-07-13 11:09:17 +0800

    sync push_value.test.js from local (fa7effa0)

--- push_value.test.js
+++ push_value.test.js
@@ -2,7 +2,7 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
 const {
-  MARKET_MAX_AGE_MS,
+  marketMaxAgeMs,
   computeMarketSnapshotId,
   validateMarketSnapshot,
   latestModelVersion,
